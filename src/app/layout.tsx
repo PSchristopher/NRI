a41f0a05dea5ef6fa9113418f0d0a3aa6f4572c9
PSchristopher <pschristopher7@gmail.com>
2024-05-14 06:22:27 -0700

    changes in home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConditionalWrapper routeName="/login">{children}</ConditionalWrapper>
+        <ConditionalWrapper routeName={["/signup", "/signin", "/verify"]}>
+          {children}</ConditionalWrapper>
       </body>
     </html>
   );
