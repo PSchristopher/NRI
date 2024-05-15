1fef5bc31f681851bf60cf56465091143167b041
PSchristopher <pschristopher7@gmail.com>
2024-05-15 13:05:51 +0530

    image slider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -41,17 +41,25 @@ const ImageSlider = () => {
 
   return (
     <div className="flex gap-4 md:gap-4 items-center justify-center w-full">
-      <button className="flex md:hidden" onClick={handlePrevClick2}>
-        <FaChevronLeft color="#6C6C6C" size={30} />
-      </button>
-      <button className="hidden md:flex " onClick={handlePrevClick}>
+      <button className="" onClick={handlePrevClick}>
         <FaChevronLeft color="#6C6C6C" size={40} />
       </button>
-      <div className="w-[60%] lg:w-1/2  bg-white block md:hidden">
+      <div className="max-w-[250px] sm:max-w-sm md:max-w-xl lg:max-w-[600px] bg-white">
         <Swiper
           slidesPerView={1}
+          spaceBetween={20}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 0,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+          }}
           loop={true}
-          ref={swiperRef2}
+          ref={swiperRef}
           className="mySwiper bg-white rounded-xl"
         >
           {[1, 2, 3, 4, 5, 6, 7, 8, 9]?.map((item, index) => (
@@ -102,67 +110,8 @@ const ImageSlider = () => {
           ))}
         </Swiper>
       </div>
-      <div className="w-[70%] lg:w-[55%]  bg-white hidden md:block">
-        <Swiper
-          slidesPerView={2}
-          spaceBetween={20}
-          loop={true}
-          ref={swiperRef}
-          className="mySwiper"
-        >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9]?.map((item, index) => (
-            <SwiperSlide className=" bg-white " key={index}>
-              <div className="bg-white rounded-xl border-2 py-12 px-6 drop-shadow-xl">
-                <Image
-                  src={"/assets/cords.png"}
-                  alt="cots"
-                  className="w-[20px]"
-                  width={500}
-                  height={1}
-                />
-                <p className="font-light   mt-4 ">
-                  Lorem ipsum dolor sit amet consectetur. Massa commodo gravida
-                  urna vel mattis. Lorem ipsum dolor sit amet consectetur. Massa
-                  commodo gravida urna vel mattis. Lorem ipsum dolor sit amet
-                  consectetur.
-                </p>
-                <div className="flex gap-4 items-center mt-10">
-                  <Image
-                    src={
-                      index % 2 === 0
-                        ? "/assets/female.png"
-                        : "/assets/male.png"
-                    }
-                    alt="cots"
-                    className="w-[60px]"
-                    width={500}
-                    height={1}
-                  />
-                  <div>
-                    <p className="font-medium text-gray-800">
-                      {index % 2 === 0 ? "Navya Roy" : "Siddharth Soni"}
-                    </p>
-                    <p className="font-medium text-gray-800  ">
-                      {index % 2 === 0 ? "Music Enthusiast" : "Audiophile"}
-                    </p>
-                    <Image
-                      src="/assets/star.png"
-                      alt="cots"
-                      className="w-[50px] mt-1"
-                      width={500}
-                      height={1}
-                    />
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <button className="flex md:hidden " onClick={handleNextClick2}>
-        <FaChevronRight color="#6C6C6C" size={30} />
-      </button>
-      <button className="hidden md:flex " onClick={handleNextClick}>
+
+      <button className="" onClick={handleNextClick}>
         <FaChevronRight color="#6C6C6C" size={40} />
       </button>
     </div>
